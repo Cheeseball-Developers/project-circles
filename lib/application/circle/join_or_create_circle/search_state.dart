eded00e2ef454330009971914f9ce3593d3cce88
Omi Wakode <omi.wakode@gmail.com>
2020-09-29 22:41:22 +0530

    fixed the bug on search screen and current circle home

--- lib/application/circle/join_or_create_circle/search_state.dart
+++ lib/application/circle/join_or_create_circle/search_state.dart
@@ -3,16 +3,15 @@ part of 'search_bloc.dart';
 @freezed
 abstract class SearchState with _$SearchState {
   const factory SearchState(
-          {@required
-              bool isSearching,
-          @required
-              bool isLoading,
-          @required
-              Option<Either<ConnectionFailure, Unit>>
-                  connectionFailureOrSuccessOption,
-          @required
-              List<User> discoveredDevices}) =
-      _SearchState; //maine code thoda alag kra h, ui m error h, wo sahi kr de phir mai connection
+      {@required
+          bool isSearching,
+      @required
+          bool isLoading,
+      @required
+          Option<Either<ConnectionFailure, Unit>>
+              connectionFailureOrSuccessOption,
+      @required
+          List<User> discoveredDevices}) = _SearchState;
 
   factory SearchState.initial() => SearchState(
       isSearching: true,
