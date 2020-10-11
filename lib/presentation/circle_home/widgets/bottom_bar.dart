9c512c5dcaae7582b7c9395b6b4ba195c7aa0fc0
Abhishek Upmanyu <upmanyu.abhishek@yahoo.in>
2020-10-11 19:07:10 +0530

    Fix issues with circle home

--- lib/presentation/circle_home/widgets/bottom_bar.dart
+++ lib/presentation/circle_home/widgets/bottom_bar.dart
@@ -1,48 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:projectcircles/application/circle/current_circle/current_circle_bloc.dart';
-import 'package:projectcircles/presentation/circle_home/widgets/pages/members_page.dart';
-import 'package:projectcircles/presentation/circle_home/widgets/pages/files_page.dart';
 
 class BottomBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: BlocConsumer<CurrentCircleBloc, CurrentCircleState>(
-        listener: (context, state) {
-          state.maybeMap(
-            hasStarted: (state) {
-              if (state.showFilesPage) {
-                showDialog(
-                  context: context,
-                  child: FilesPage(),
-                );
-              }
-              if (state.showMembersPage) {
-                showDialog(
-                  context: context,
-                  child: MembersPage(),
-                );
-              }
-            },
-            hasJoined: (state) {
-              if (state.showFilesPage) {
-                showDialog(
-                  context: context,
-                  child: FilesPage(),
-                );
-              }
-              if (state.showMembersPage) {
-                showDialog(
-                  context: context,
-                  child: MembersPage(),
-                );
-              }
-            },
-            orElse: null,
-          );
-        },
+      child: BlocBuilder<CurrentCircleBloc, CurrentCircleState>(
         builder: (context, state) => Stack(
           children: [
             Padding(
@@ -58,20 +23,22 @@ class BottomBar extends StatelessWidget {
                     IconButton(
                       icon: const Icon(Icons.save_alt),
                       color: Theme.of(context).accentIconTheme.color,
-                      onPressed: () => context.bloc<CurrentCircleBloc>().add(const CurrentCircleEvent.showFilesPage()),
+                      onPressed: () => context
+                          .bloc<CurrentCircleBloc>()
+                          .add(const CurrentCircleEvent.showFilesPage()),
                     ),
                     IconButton(
                       icon: const Icon(Icons.send),
-                      onPressed: () {
-                        context.bloc<CurrentCircleBloc>().add(
-                              const CurrentCircleEvent.sendFiles(),
-                            );
-                      },
+                      onPressed: () => context.bloc<CurrentCircleBloc>().add(
+                            const CurrentCircleEvent.sendFiles(),
+                          ),
                     ),
                     IconButton(
                       icon: const Icon(Icons.people),
                       color: Theme.of(context).accentIconTheme.color,
-                      onPressed: () => context.bloc<CurrentCircleBloc>().add(const CurrentCircleEvent.showMembersPage()),
+                      onPressed: () => context
+                          .bloc<CurrentCircleBloc>()
+                          .add(const CurrentCircleEvent.showMembersPage()),
                     ),
                   ],
                 ),
