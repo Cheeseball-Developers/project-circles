4ee6abd37dfa2e0cc4ca758f46e2e749ec68ae84
Abhishek Upmanyu <upmanyu.abhishek@yahoo.in>
2020-12-29 17:21:00 +0530

    Add progress UI for file transfer

--- lib/presentation/core/widgets/my_list_tile.dart
+++ lib/presentation/core/widgets/my_list_tile.dart
@@ -5,6 +5,7 @@ class MyListTile extends StatelessWidget {
   final Widget trailing;
   final String title;
   final String subtitle;
+  final double progress;
   final bool toggleValue;
   final VoidCallback onTap;
 
@@ -13,6 +14,7 @@ class MyListTile extends StatelessWidget {
       @required this.leading,
       @required this.title,
       this.subtitle,
+      this.progress = 0.0,
       this.toggleValue = true,
       this.onTap,
       this.trailing})
@@ -24,30 +26,46 @@ class MyListTile extends StatelessWidget {
       color: Colors.white,
       child: InkWell(
         onTap: onTap,
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Row(
-            children: [
-              leading,
-              const Padding(padding: EdgeInsets.only(right: 16.0)),
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(title, style: Theme.of(context).textTheme.subtitle1),
-                    if (subtitle != null)
-                      Padding(
-                        padding: const EdgeInsets.only(top: 2.0),
-                        child: Text(subtitle,
-                            style: Theme.of(context).textTheme.bodyText2),
-                      )
-                  ],
-                ),
+        child: Stack(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: ShaderMask(
+                    shaderCallback: (rect) => LinearGradient(
+                        colors: [Theme.of(context).accentColor],
+                        stops: [0.0, progress]).createShader(rect),
+                  ),
+                )
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  leading,
+                  const Padding(padding: EdgeInsets.only(right: 16.0)),
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(title,
+                            style: Theme.of(context).textTheme.subtitle1),
+                        if (subtitle != null)
+                          Padding(
+                            padding: const EdgeInsets.only(top: 2.0),
+                            child: Text(subtitle,
+                                style: Theme.of(context).textTheme.bodyText2),
+                          )
+                      ],
+                    ),
+                  ),
+                  trailing,
+                ],
               ),
-              trailing,
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
